772331d9c54c7e48fd793f58da4a32fcd7efa784
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2024-11-26 21:19:44 +0530

    Setting up the backend...

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import helmet from "helmet";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8800;
